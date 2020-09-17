783e13aef158836e63fb202dffa7cff95ec96d0e
XavierM <xavier.martinez.xm@gmail.com>
2020-09-17 14:24:04 +0200

    Dont use MLAPI NAT puncher server for testing + Fix a typo

--- MLAPI.Puncher.Client.Console/Program.cs
+++ MLAPI.Puncher.Client.Console/Program.cs
@@ -7,7 +7,7 @@ namespace MLAPI.Puncher.Client.Console
 {
     class Program
     {
-        public const string PUNCHER_SERVER_HOST = "puncher.midlevel.io";
+        public const string PUNCHER_SERVER_HOST = "127.0.0.1";
         public const int PUNCHER_SERVER_PORT = 6776;
 
         static void Main(string[] args)
